41d84e4189606981006928eee9fa1d500f5e9d65
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-02 08:42:29 -0700

    update promiseall.js

--- promiseall.js
+++ promiseall.js
@@ -1,16 +1,14 @@
 // asynchrounously read a file
-const fs = require('fs')
-const util = require('util')
-const readFile = fs.readFile
-const promisify = util.promisify
+const { readFile } = require('fs')
+const { promisify } = require('util')
 
 // use Node.js' util.promisify to make a function (that takes a callback) use Promises
-const readPromise = promisify(readFile)
+const readfile = promisify(readFile)
 
-const readAnimals = readPromise('animals.txt', 'utf-8')
-const readTrees = readPromise('trees.txt', 'utf-8')
+const readAnimals = readfile('animals.txt', 'utf-8')
+const readTrees = readfile('trees.txt', 'utf-8')
 
-// use Promis.all to run asynchronous code in parallel
+// use Promise.all to run asynchronous code in parallel
 const readFiles = Promise.all([readAnimals, readTrees])
 
 readFiles
